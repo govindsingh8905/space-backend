bb094646b5b3fea3c481fd22861eb9fedb88f276
GOVIND SINGH <govindsingh@GOVINDs-MacBook-Air.local>
2026-06-10 17:28:03 +0530

    fix: configure netlify build for orbital-debris and remove unused webpack shader loaders

--- orbital-debris/next.config.js
+++ orbital-debris/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['three'],
-  webpack: (config) => {
-    // Handle GLSL shader files
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
